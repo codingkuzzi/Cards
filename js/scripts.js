dc5e55b70a535c5e35108e780153ca2d8c7adc17
Anna Kuznetsova and Joel Bakken <anna@>
2017-06-05 16:33:37 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+  var ranks = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+  var suits = ['clubs','diamonds','spades','hearts'];
+
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      $("ul").append("li class=\"" + suit + "\">" + rank + " of " + suit + "</li>");
+    });
+  });
+  
+});
