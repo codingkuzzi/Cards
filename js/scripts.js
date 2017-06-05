043bed43d523c46ab5cba00f450ccf103416e765
Anna Kuznetsova and Joel Bakken <anna@>
2017-06-05 16:45:05 -0700

    put them in correct order

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
 
   var ranks = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
-  var suits = ['clubs','diamonds','spades','hearts'];
+  var suits = ['hearts','clubs','diamonds','spades'];
 
   suits.forEach(function(suit) {
     ranks.forEach(function(rank) {
-      $("ul").append("li class=\"" + suit + "\">" + rank + " of " + suit + "</li>");
+      $("ul").append('<li class="' + suit + '">' + rank + " of " + suit + "</li>");
     });
   });
-  
+
 });
